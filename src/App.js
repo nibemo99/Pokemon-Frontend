45e65d2783192b7fae7ffd756020377e10d6b35c
nibemo99 <49243236+nibemo99@users.noreply.github.com>
2022-10-09 03:42:07 -0500

    Background animation done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,61 @@
+import { useEffect, useState } from 'react';
+import { motion, AnimatePresence } from 'framer-motion'
 import s from './Styles/App.module.css'
+import { imgs } from './Utils/Backgrounds'
+import { isVisible } from '@testing-library/user-event/dist/utils';
 
 
 function App () {
+  const [backgroundIndex, setBackgroundIndex] = useState( { index: 0, isVisible: true } )
+
+  useEffect( () => {
+
+    const timer = setInterval( () => {
+      setBackgroundIndex( { ...backgroundIndex, isVisible: false } )
+      setTimeout( () => {
+        const newIndex = ( backgroundIndex.index + 1 ) % imgs.length
+        setBackgroundIndex( { index: newIndex, isVisible: true } )
+
+      }, 3000 );
+    }, 3200 );
+    return () => {
+      clearInterval( timer )
+    }
+  }, [backgroundIndex] )
+
+
+
+
+
   return (
     <div className={s.wrapper}>
 
-      hola
+      <div className={s.background}>
+        {/* <div className={s.backdrop} /> */}
 
+        <AnimatePresence>
+          {backgroundIndex.isVisible &&
+            <motion.img
+              initial={{ scale: 1, opacity: 0 }}
+              animate={{ scale: 1.1, opacity: 1 }}
+              exit={{ scale: 1.2, opacity: 0, }}
+              transition={{ duration: 3 }}
+              className={s.image}
+              alt=''
+              src={imgs[backgroundIndex.index]}
+            />}
+        </AnimatePresence>
+      </div>
+
+      <div className={s.section}>
+        <p className={s.text}>
+          Welcome to PokeWiki
+        </p>
+      </div>
+
+      <p className={s.click}>
+        Click anywhere to continue
+      </p>
     </div>
   );
 }
---
+++ src/Utils/Backgrounds.js
@@ -0,0 +1,23 @@
+import charmander from '../Assets/Backgrounds/charmander.jpg'
+import mewtwo from '../Assets/Backgrounds/149386.jpg'
+import mewtwo_dark from '../Assets/Backgrounds/mewtwo_dark.jpg'
+import polygon from '../Assets/Backgrounds/polygon.jpg'
+import pony from '../Assets/Backgrounds/pony.jpg'
+import raichu from '../Assets/Backgrounds/raichu.jpg'
+import Starmie from '../Assets/Backgrounds/Starmie.jpg'
+import white from '../Assets/Backgrounds/white.jpg'
+import zapdos from '../Assets/Backgrounds/zapdos.jpg'
+
+
+
+export const imgs = [
+    charmander,
+    mewtwo,
+    mewtwo_dark,
+    polygon,
+    pony,
+    raichu,
+    Starmie,
+    white,
+    zapdos,
+]
