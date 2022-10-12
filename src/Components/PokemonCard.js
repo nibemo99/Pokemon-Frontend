ea7a4b5cc9b58fdefbc4212fdfc4e8cecfc489db
nibemo99 <49243236+nibemo99@users.noreply.github.com>
2022-10-12 00:46:00 -0500

    Changed image source to pokeapi official-artwork

--- src/Components/PokemonCard.js
+++ src/Components/PokemonCard.js
@@ -3,31 +3,21 @@ import s from '../Styles/PokemonCard.module.css'
 
 const PokemonCard = ( { pokemon } ) => {
 
-    const formatId = ( id ) => {
-        if ( id < 10 ) {
-            return `00${id}`
-        } else if ( id < 100 ) {
-            return `0${id}`
-        } else {
-            return id
-        }
 
 
-    }
-
-    const image = `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${formatId( pokemon.id )}.png`
 
     const capFirstLetter = ( name ) => {
         return name.replace( name[0], name[0].toUpperCase() )
     }
+
+
     return (
         <div className={s.individualCard}>
             <div className={s.cardTitle}>
-
                 <p>{capFirstLetter( pokemon.name )}</p>
                 <p>{pokemon.id}</p>
             </div>
-            <img className={s.miniImage} alt='' src={image} />
+            <img className={s.miniImage} alt='' src={pokemon.image} />
             <div className={s.cardTypes}>
                 Feugo
             </div>
--- src/Pages/Page1.js
+++ src/Pages/Page1.js
@@ -4,25 +4,38 @@ import AnimatedPage from './AnimatedPage'
 import s from '../Styles/Page1.module.css'
 import { useNavigate } from 'react-router';
 import CardsContainer from '../Components/CardsContainer';
-import { addPokemonsAPI } from '../Redux/Actions';
+import { addPokemonsAPI, appendPokemonsAPI } from '../Redux/Actions';
 import { useDispatch } from 'react-redux';
 
 const Page1 = () => {
     const navigate = useNavigate();
     const dispatch = useDispatch()
-
+    const POKEMONS_PER_PAGE = 12
 
     useEffect( () => {
-        const pokemons = []
+        let pokemons = []
         const fetchData = async () => {
             try {
-                for ( let i = 1; i < 25; i++ ) {
+                for ( let i = 1; i <= POKEMONS_PER_PAGE; i++ ) {
                     let res = await fetch( `https://pokeapi.co/api/v2/pokemon/${i}` )
                     let json = await res.json()
-                    let { id, name, height, weight, stats, types } = json
-                    pokemons.push( { id, name, height, weight, stats, types } )
+                    let { id, name, height, weight, stats, types, sprites } = json
+                    let image = sprites.other["official-artwork"].front_default
+                    pokemons.push( { id, name, height, weight, image, stats, types } )
                 }
                 dispatch( addPokemonsAPI( pokemons ) )
+                setTimeout( async () => {
+                    pokemons = []
+                    for ( let i = POKEMONS_PER_PAGE * 12 + 1; i <= POKEMONS_PER_PAGE * 20; i++ ) {
+                        let res = await fetch( `https://pokeapi.co/api/v2/pokemon/${i}` )
+                        let json = await res.json()
+                        let { id, name, height, weight, stats, types, sprites } = json
+                        let image = sprites.other["official-artwork"].front_default
+                        pokemons.push( { id, name, height, weight, image, stats, types } )
+                    }
+                    dispatch( addPokemonsAPI( pokemons ) )
+
+                }, 500 );
                 console.log( pokemons )
             } catch ( error ) {
                 console.log( error )
@@ -30,7 +43,7 @@ const Page1 = () => {
         }
         fetchData()
         return () => { }
-    }, [] )
+    }, [dispatch] )
 
     const imagen = `https://assets.pokemon.com/assets/cms2/img/pokedex/full/150.png`
 
