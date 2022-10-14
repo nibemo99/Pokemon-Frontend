a179343825778d4d8caaf026516410925c00427a
nibemo99 <49243236+nibemo99@users.noreply.github.com>
2022-10-14 00:11:51 -0500

    Added background

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { setPageTo } from '../Redux/Actions'
 import s from '../Styles/Navbar.module.css'
+import { motion } from "framer-motion";
 
 const Navbar = ( { pokemonsPerPage } ) => {
     const dispatch = useDispatch()
@@ -26,28 +27,96 @@ const Navbar = ( { pokemonsPerPage } ) => {
         dispatch( setPageTo( number ) )
     }
 
+    // Animations
+    const animations = {
+        whileHover: {
+            scale: 1.2,
+        },
+        whileTap: {
+            scale: 0.9,
+        },
+    }
+
 
     return (
         <div className={s.navbar}>
-            <svg onClick={handlePrevious} className={s.icons} fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M11 15l-3-3m0 0l3-3m-3 3h8M3 12a9 9 0 1118 0 9 9 0 01-18 0z"></path></svg>
+            <motion.svg
+                variants={animations}
+                whileHover='whileHover'
+                whileTap='whileTap'
+                onClick={handlePrevious}
+                className={s.icons}
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+            >
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M11 15l-3-3m0 0l3-3m-3 3h8M3 12a9 9 0 1118 0 9 9 0 01-18 0z"></path>
+            </motion.svg>
 
-            <button onClick={handlePage} className={`${s.button} ${( currentPage === 1 ) ? ( s.selected ) : ''}`} >
+            <motion.button
+                variants={animations}
+                whileHover='whileHover'
+                whileTap='whileTap'
+                onClick={handlePage}
+                className={`${s.button} ${( currentPage === 1 ) ? ( s.selected ) : ''}`}
+            >
                 {( currentPage >= 4 ) ? ( currentPage - 2 ) : '1'}
-            </button>
-            <button onClick={handlePage} className={`${s.button} ${( currentPage === 2 ) ? ( s.selected ) : ''}`} >
-                {( currentPage >= 4 ) ? ( currentPage - 1 ) : '2'}
-            </button>
-            <button onClick={handlePage} className={`${s.button} ${( currentPage > 2 ) ? ( s.selected ) : ''}`} >
-                {( currentPage >= 4 ) ? ( currentPage ) : '3'}
-            </button>
-            <button onClick={handlePage} className={`${s.button} `} >
-                {( currentPage >= 4 ) ? ( currentPage + 1 ) : '4'}
-            </button>
-            <button onClick={handlePage} className={`${s.button} `} >
-                {( currentPage >= 4 ) ? ( currentPage + 2 ) : '5'}
-            </button>
+            </motion.button>
 
-            <svg onClick={handleNext} className={s.icons} fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 9l3 3m0 0l-3 3m3-3H8m13 0a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+            <motion.button
+                variants={animations}
+                whileHover='whileHover'
+                whileTap='whileTap'
+                onClick={handlePage}
+                className={`${s.button} ${( currentPage === 2 ) ? ( s.selected ) : ''}`}
+            >
+                {( currentPage >= 4 ) ? ( currentPage - 1 ) : '2'}
+            </motion.button>
+
+            <motion.button
+                variants={animations}
+                whileHover='whileHover'
+                whileTap='whileTap'
+                onClick={handlePage}
+                className={`${s.button} ${( currentPage > 2 ) ? ( s.selected ) : ''}`}
+            >
+                {( currentPage >= 4 ) ? ( currentPage ) : '3'}
+            </motion.button>
+
+            <motion.button
+                variants={animations}
+                whileHover='whileHover'
+                whileTap='whileTap'
+                onClick={handlePage}
+                className={`${s.button} `}
+            >
+                {( currentPage >= 4 ) ? ( currentPage + 1 ) : '4'}
+            </motion.button>
+
+            <motion.button
+                variants={animations}
+                whileHover='whileHover'
+                whileTap='whileTap'
+                onClick={handlePage}
+                className={`${s.button} `}
+            >
+                {( currentPage >= 4 ) ? ( currentPage + 2 ) : '5'}
+            </motion.button>
+
+            <motion.svg
+                variants={animations}
+                whileHover='whileHover'
+                whileTap='whileTap'
+                onClick={handleNext}
+                className={s.icons}
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+            >
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 9l3 3m0 0l-3 3m3-3H8m13 0a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+            </motion.svg>
         </div>
     )
 }
