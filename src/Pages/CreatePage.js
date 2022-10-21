345e2123c84543f9618a585c94a504cefc9e1e63
nibemo99 <49243236+nibemo99@users.noreply.github.com>
2022-10-21 02:22:03 -0500

    Created clear form fields

--- src/Pages/CreatePage.js
+++ src/Pages/CreatePage.js
@@ -13,12 +13,14 @@ import Attack from '../Assets/Icons/sword-single.svg'
 import Vertical from '../Assets/Icons/vertical.svg'
 import Speed from '../Assets/Icons/speed.svg'
 import Types from '../Components/Types';
+import notfound from '../Assets/notfound-compressed.png'
 
 const Create = () => {
     const dispatch = useDispatch()
     const navigate = useNavigate()
     const [removing, setRemoving] = useState( false )
     const [form, setForm] = useState( {
+        url: '',
         id: '',
         name: '',
         height: '',
@@ -46,75 +48,82 @@ const Create = () => {
         return name.replace( name[0], name[0].toUpperCase() )
     }
 
+    const handleCreate = ( event ) => {
 
-
-    const detail = {
-        "id": 10,
-        "name": "caterpie",
-        "height": 3,
-        "weight": 29,
-        "image": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/10.png",
-        "stats": [
-            {
-                "base_stat": 45,
-                "effort": 1,
-                "stat": {
-                    "name": "hp",
-                    "url": "https://pokeapi.co/api/v2/stat/1/"
-                }
-            },
-            {
-                "base_stat": 30,
-                "effort": 0,
-                "stat": {
-                    "name": "attack",
-                    "url": "https://pokeapi.co/api/v2/stat/2/"
-                }
-            },
-            {
-                "base_stat": 35,
-                "effort": 0,
-                "stat": {
-                    "name": "defense",
-                    "url": "https://pokeapi.co/api/v2/stat/3/"
-                }
-            },
-            {
-                "base_stat": 20,
-                "effort": 0,
-                "stat": {
-                    "name": "special-attack",
-                    "url": "https://pokeapi.co/api/v2/stat/4/"
-                }
-            },
-            {
-                "base_stat": 20,
-                "effort": 0,
-                "stat": {
-                    "name": "special-defense",
-                    "url": "https://pokeapi.co/api/v2/stat/5/"
-                }
-            },
-            {
-                "base_stat": 45,
-                "effort": 0,
-                "stat": {
-                    "name": "speed",
-                    "url": "https://pokeapi.co/api/v2/stat/6/"
-                }
-            }
-        ],
-        "types": [
-            {
-                "slot": 1,
-                "type": {
-                    "name": "bug",
-                    "url": "https://pokeapi.co/api/v2/type/7/"
-                }
-            }
-        ]
     }
 
+
+
+
+    const handleOnChange = ( event ) => {
+        const field = event.target.name
+        console.log( event.target.value, typeof event.target.value );
+        switch ( field ) {
+            case 'url':
+                setForm( prev => ( { ...form, url: event.target.value } ) )
+                break;
+            case 'name':
+                setForm( prev => ( { ...form, name: event.target.value } ) )
+                break;
+            case 'id':
+                setForm( prev => ( { ...form, id: event.target.value } ) )
+                break;
+            case 'height':
+                setForm( prev => ( { ...form, height: event.target.value } ) )
+                break;
+            case 'weight':
+                setForm( prev => ( { ...form, weight: event.target.value } ) )
+                break;
+            case 'hp':
+                setForm( prev => ( { ...form, hp: event.target.value } ) )
+                break;
+            case 'attack':
+                setForm( prev => ( { ...form, attack: event.target.value } ) )
+                break;
+            case 'defense':
+                setForm( prev => ( { ...form, defense: event.target.value } ) )
+                break;
+            case 'specialAttack':
+                setForm( prev => ( { ...form, specialAttack: event.target.value } ) )
+                break;
+            case 'specialDefense':
+                setForm( prev => ( { ...form, specialDefense: event.target.value } ) )
+                break;
+            case 'speed':
+                setForm( prev => ( { ...form, speed: event.target.value } ) )
+                break;
+            case 'type1':
+                setForm( prev => ( { ...form, type1: event.target.value } ) )
+                break;
+            case 'type2':
+                setForm( prev => ( { ...form, type2: event.target.value } ) )
+                break;
+            default:
+                break;
+        }
+    }
+
+    const handleClear = ( event ) => {
+        setForm( prev => ( {
+            url: '',
+            id: '',
+            name: '',
+            height: '',
+            weight: '',
+            hp: '',
+            attack: '',
+            defense: '',
+            specialAttack: '',
+            specialDefense: '',
+            speed: '',
+            type1: '',
+            type2: '',
+        } ) )
+    }
+
+    const handleRandom = ( event ) => {
+
+    }
 
     return (
         <AnimatedPage2 removing={removing}>
@@ -130,59 +139,66 @@ const Create = () => {
                 </p>
 
                 <div className={s.card}>
-                    <img alt='' className={s.image} src={detail.image} />
+                    <div>
+                        <img alt='' className={s.image} src={form.url || notfound} />
+                        <div className={s.buttons}>
+                            <div className={s.inputWrapper3} ><input onChange={handleOnChange} value={form.url} name='url' className={s.wh} type='text' placeholder='Image URL' /></div>
+                            <button onClick={handleClear} className={s.cr} >Clear</button>
+                            <button onClick={handleRandom} className={s.cr} >Random</button>
+                        </div>
+                    </div>
                     <div className={s.info}>
                         <div className={s.title}>
-                            <div className={s.inputWrapper} ><input className={s.name} type='text' placeholder='Name' /></div>
-                            <div className={s.inputWrapper} ><input className={s.id} type='text' placeholder='ID' /></div>
+                            <div className={s.inputWrapper} ><input onChange={handleOnChange} value={form.name} name='name' className={s.name} type='text' placeholder='Name' /></div>
+                            <div className={s.inputWrapper} ><input onChange={handleOnChange} value={form.id} name='id' className={s.id} type='text' placeholder='ID' /></div>
                         </div>
                         <div className={s.stats}>
                             <div>
                                 <img alt='' className={s.icons} src={Vertical} />
                                 Height:
-                                <div className={s.inputWrapper2} ><input className={s.wh} type='text' placeholder='1' /></div>
+                                <div className={s.inputWrapper2} ><input onChange={handleOnChange} value={form.height} name='height' className={s.wh} type='text' placeholder='1' /></div>
                             </div>
                             <div>
                                 <img alt='' className={s.icons} src={Weight} />
                                 Weight:
-                                <div className={s.inputWrapper2} ><input className={s.wh} type='text' placeholder='1' /></div>
+                                <div className={s.inputWrapper2} ><input onChange={handleOnChange} value={form.weight} name='weight' className={s.wh} type='text' placeholder='1' /></div>
                             </div>
                         </div>
                         <div className={s.stats} >
                             <div>
                                 <img alt='' className={s.icons} src={Hp} />
                                 HP:
-                                <div className={s.inputWrapper2} ><input className={s.wh} type='text' placeholder='1' /></div>
+                                <div className={s.inputWrapper2} ><input onChange={handleOnChange} value={form.hp} name='hp' className={s.wh} type='text' placeholder='1' /></div>
                             </div>
                             <div>
                                 <img alt='' className={s.icons} src={Attack} />
                                 Attack:
-                                <div className={s.inputWrapper2} ><input className={s.wh} type='text' placeholder='1' /></div>
+                                <div className={s.inputWrapper2} ><input onChange={handleOnChange} value={form.attack} name='attack' className={s.wh} type='text' placeholder='1' /></div>
                             </div>
                             <div>
                                 <img alt='' className={s.icons} src={Defense} />
                                 Defense:
-                                <div className={s.inputWrapper2} ><input className={s.wh} type='text' placeholder='1' /></div>
+                                <div className={s.inputWrapper2} ><input onChange={handleOnChange} value={form.defense} name='defense' className={s.wh} type='text' placeholder='1' /></div>
                             </div>
                             <div>
                                 <img alt='' className={s.icons} src={SpecialAttack} />
                                 Special Attack:
-                                <div className={s.inputWrapper2} ><input className={s.wh} type='text' placeholder='1' /></div>
+                                <div className={s.inputWrapper2} ><input onChange={handleOnChange} value={form.specialAttack} name='specialAttack' className={s.wh} type='text' placeholder='1' /></div>
                             </div>
                             <div>
                                 <img alt='' className={s.icons} src={SpecialDefense} />
                                 Special Defense:
-                                <div className={s.inputWrapper2} ><input className={s.wh} type='text' placeholder='1' /></div>
+                                <div className={s.inputWrapper2} ><input onChange={handleOnChange} value={form.specialDefense} name='specialDefense' className={s.wh} type='text' placeholder='1' /></div>
                             </div>
                             <div>
                                 <img alt='' className={s.icons} src={Speed} />
                                 Speed:
-                                <div className={s.inputWrapper2} ><input className={s.wh} type='text' placeholder='1' /></div>
+                                <div className={s.inputWrapper2} ><input onChange={handleOnChange} value={form.speed} name='speed' className={s.wh} type='text' placeholder='1' /></div>
                             </div>
                         </div>
                         <div className={s.types}>
-                            <input className={s.type1} placeholder='Type 1' list="types" />
-                            <input className={s.type1} placeholder='Type 2' list="types" />
+                            <input onChange={handleOnChange} value={form.type1} name='type1' className={s.type1} placeholder='Type 1' list="types" />
+                            <input onChange={handleOnChange} value={form.type2} name='type2' className={s.type1} placeholder='Type 2' list="types" />
                             <datalist id="types">
                                 <option value='Grass' />
                                 <option value='Poison' />
@@ -204,6 +220,8 @@ const Create = () => {
                                 <option value='Dark' />
                             </datalist>
                         </div>
+
+                        <button onClick={handleCreate} className={s.create}>Create</button>
 
                     </div>
                 </div>
